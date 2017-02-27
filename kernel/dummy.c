a3e1916799b9b7cff27c429d3a0c742ad32275a3
Pavel Vorobyov <aquavitale@yandex.ru>
2017-02-28 00:24:26 +0300

    merge

--- kernel/dummy.c
+++ kernel/dummy.c
@@ -1,5 +1,5 @@
 #include "string.h"
-#include "dummy.h"
+#include "stdio.h"
 
 void cls() {
 	outp(254, 0); // border 0
@@ -16,10 +16,5 @@ void dummy1() {
 }
 
 void dummy2() {
-	unsigned char f = 1;
-	cls();
-	while (1) {
-		memset(0x4800, f, 0x800);
-		f++;
-	}
+    printf("Abc");
 }
