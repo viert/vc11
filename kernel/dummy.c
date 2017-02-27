9b5ed87701715c031d515466389cfea2cf6bdf52
Pavel Vorobyov <aquavitale@yandex.ru>
2017-02-28 00:23:19 +0300

    io started

--- kernel/dummy.c
+++ kernel/dummy.c
@@ -1,5 +1,6 @@
 #include "string.h"
 #include "dummy.h"
+#include "stdio.h"
 
 void cls() {
 	outp(254, 0); // border 0
@@ -16,10 +17,11 @@ void dummy1() {
 }
 
 void dummy2() {
-	unsigned char f = 1;
-	cls();
-	while (1) {
-		memset(0x4800, f, 0x800);
-		f++;
-	}
+	// unsigned char f = 1;
+	// cls();
+	// while (1) {
+	// 	memset(0x4800, f, 0x800);
+	// 	f++;
+	// }
+	printf("Blah, minor");
 }
---
+++ kernel/io/putc.h
@@ -0,0 +1,6 @@
+#ifndef __PUTC_H__
+#define __PUTC_H__
+
+extern void		__FASTCALL__	putc(unsigned char code);
+
+#endif
